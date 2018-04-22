35fa5b4777fca03ace6372e2f994f242c58b7acb
Noel De Martin <noeldemartin@gmail.com>
2018-04-22 11:52:49 +0200

    Add SPA files

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-grey-light w-screen h-screen flex items-center justify-center">
+        <h1>Github Projects Browser</h1>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
